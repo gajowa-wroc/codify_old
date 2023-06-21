9ad0709c98fd163568e373d480e54f296044e278
necip-yolcu <ncpyolcu@gmail.com>
2023-06-21 13:46:40 +0200

    fix: MainPage conflict has resolved

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TerminalWrite from './TerminalWrite'
+import { useDispatch } from 'react-redux';
+import { getDatabase } from 'firebase/database';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { setUser } from '../userSlice';
+import { auth } from '../firebase_connect';
 
 const MainPage = () => {
+  const dispatch = useDispatch()
+
+  const database = getDatabase();
+  //const auth = useSelector(state => state.firebase.auth)
+  //console.log("auth__:",auth)
+
+  const email = "hey1@gmail.com"
+  const password = "123456"
+  const firstname = 'gajowa_tikka'
+  const lastname = 'wroc_tikka'
+
+
+  useEffect(() => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(({user}) => {
+        dispatch(setUser({
+          id: user.uid,
+          name: user.displayName,
+          email: user.email,
+          token: user.accessToken,
+          email_verified: user.emailVerified
+        }))
+        console.log("createUserWithEmailAndPassword",user)
+      })
+      .catch(err => console.log("mtfireErr:",err))
+
+      //writeUserData(23, "ahmet", "ahmet1@gmail.com", "https://fastly.picsum.photos/id/1074/5000/3333.jpg?hmac=WTLC8LlWe9c9_kJZsPLer9EWSoaFo5MpRIePupGhzlo")
+  }, [])
+
+  //signInWithEmailAndPassword()
+
   return (
     <React.Fragment>
       <section className='grid place-items-center'>
