dd3683dc00ea57d4a6f11214e544c5386bc3f144
tikkaaa3 <tikkaaa3@gmail.com>
2023-06-21 15:03:19 +0200

    padding added, terminalRead and functions files has added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import TerminalWrite from './TerminalWrite'
+import Functions from './Functions'
+import TerminalRead from './TerminalRead'
 
 const MainPage = () => {
   return (
     <React.Fragment>
-      <section className='grid place-items-center'>
-        <div className='w-screen h-screen grid lg:grid-cols-2 md:grid-cols-2 md:grid-cols-2 xs:grid-rows-2 text-red-900'>
-          <div className='w-full min-h-screen bg-blue-800 centered md:h-screen  order-last md:order-first'>
+      <section className='grid place-items-center input-cursor'>
+        <div className='w-screen h-screen grid lg:grid-cols-2 md:grid-cols-2 md:grid-cols-2 xs:grid-rows-2 '>
+          <div className='w-full min-h-screen bg-blue-800 centered md:h-screen  order-last md:order-first  border border-stone-200 border-2 text-white'>
             <TerminalWrite/>
           </div>
 
-          <div className='w-full min-h-screen bg-black centered md:h-screen md:order-last'>
-            <p>Terminalr</p>
+          <div className='w-full min-h-screen bg-stone-950 centered md:h-screen md:order-last border border-sky-600 border-2 text-teal-600'>
+            <TerminalRead/>
           </div>
         </div>
       </section>
---
+++ src/components/TerminalRead.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Functions from './Functions'
+
+const TerminalRead = () => {
+    //const, let...
+
+    return(
+        <div className='w-full h-full bg-stone-950 overflow-auto md:overflow-y-auto flex flex-col'>
+            <span className='pl-2 pt-2'>Messages will appear here!</span>
+        </div>
+    )
+}
+
+export default TerminalRead
